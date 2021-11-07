39a2eedd1433d399e3bfc177a3aa846e3acccdfb
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-06 21:24:36 -0400

    added counterby

--- src/bases/CounterBy.tsx
+++ src/bases/CounterBy.tsx
@@ -3,18 +3,30 @@ import { useState } from "react";
 interface Props {
     initialValue?:number
 }
+interface CounterState {
+    counter:number;
+    clicks:number;
+}
 
 const CounterBy = ({initialValue= 5}: Props) => {
 
-    const [counter, setCounter] =useState(initialValue);
+    const [{counter, clicks}, setCounter] =useState<CounterState>({
+        counter: initialValue,
+        clicks: 0,
+    });
 
-    const handleClick = () => {
-        setCounter(prev=> prev + 1 );
+    const handleClick = (value: number) => {
+        setCounter(prev=>({
+            counter: prev.counter + value,
+            clicks: prev.clicks + 1
+        }));
     }
     return (
         <>
-           <h1>Counter: {counter}</h1> 
-           <button onClick={handleClick}>+1</button>
+           <h1>CounterBy: {counter}</h1> 
+           <h1>Cliks: {clicks}</h1> 
+           <button onClick={()=> handleClick(1)}>+1</button>
+           <button onClick={()=>handleClick(5)}>+5</button>
         </>
     )
 }
