cefa3b0d507401d728c87530b16370c639e1fb0c
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-06 20:50:20 -0400

    first commit

---
+++ src/bases/CounterBy.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+interface Props {
+    initialValue?:number
+}
+
+const CounterBy = ({initialValue= 5}: Props) => {
+
+    const [counter, setCounter] =useState(initialValue);
+
+    const handleClick = () => {
+        setCounter(prev=> prev + 1 );
+    }
+    return (
+        <>
+           <h1>Counter: {counter}</h1> 
+           <button onClick={handleClick}>+1</button>
+        </>
+    )
+}
+
+export default CounterBy;
